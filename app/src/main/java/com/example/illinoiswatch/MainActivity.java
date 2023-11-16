98f37a921dd3214d609d9aa7ca15c7caca574ea1
peterzhu <z18819828123@gmail.com>
2023-11-15 22:02:33 -0600

    Backend done

--- app/src/main/java/com/example/illinoiswatch/MainActivity.java
+++ app/src/main/java/com/example/illinoiswatch/MainActivity.java
@@ -1,20 +1,10 @@
 package com.example.illinoiswatch;
 
-import static android.content.ContentValues.TAG;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 public class MainActivity extends AppCompatActivity {
-    private BroadcastReceiver alertReceiver;
-
 
 
     @Override
@@ -22,39 +12,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        alertReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                Log.d(TAG, "test from activity" );
-
-                if ("com.example.ACTION_SMS_ALERT".equals(intent.getAction())) {
-                    AlertDetails details = (AlertDetails) intent.getSerializableExtra("alertDetails");
-                    if (details != null) {
-                        Log.d(TAG, "test from activity" + details.getEventType());
-                        Log.d(TAG,"test from activity"+ details.getAddress());
-
-
-
-                        // Use the details here as needed, e.g., update the UI
-                    }
-                }
-            }
-
-        };
-
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // Register BroadcastReceiver
-        IntentFilter filter = new IntentFilter("com.example.ACTION_SMS_ALERT");
-        LocalBroadcastManager.getInstance(this).registerReceiver(alertReceiver, filter);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        // Unregister BroadcastReceiver
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(alertReceiver);
     }
 }
