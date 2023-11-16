3200b4013521d05acac1a017636ba82c7fbe9e97
peterzhu <z18819828123@gmail.com>
2023-11-15 22:03:01 -0600

    Backend done

---
+++ app/src/main/java/com/example/illinoiswatch/SmsActivity.java
@@ -0,0 +1,58 @@
+package com.example.illinoiswatch;
+
+import static android.content.ContentValues.TAG;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+public class SmsActivity extends AppCompatActivity {
+    private BroadcastReceiver alertReceiver;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        alertReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d(TAG, "test from activity" );
+
+                if ("com.example.ACTION_SMS_ALERT".equals(intent.getAction())) {
+                    AlertDetails details = (AlertDetails) intent.getSerializableExtra("alertDetails");
+                    if (details != null) {
+                        Log.d(TAG, "test from activity " + details.getEventType());
+                        Log.d(TAG,"test from activity "+ details.getAddress());
+
+                    }
+                }
+            }
+
+        };
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Register BroadcastReceiver
+        IntentFilter filter = new IntentFilter("com.example.ACTION_SMS_ALERT");
+        LocalBroadcastManager.getInstance(this).registerReceiver(alertReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Unregister BroadcastReceiver
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(alertReceiver);
+    }
+
+}
