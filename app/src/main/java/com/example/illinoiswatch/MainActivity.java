11da4dce1ab741ad9eae34382f66cd8e6d43399b
Alex Tanthiptham <jirawatcharat@gmail.com>
2023-11-16 22:44:06 -0600

    Navitation done

--- app/src/main/java/com/example/illinoiswatch/MainActivity.java
+++ app/src/main/java/com/example/illinoiswatch/MainActivity.java
@@ -5,12 +5,11 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import com.example.illinoiswatch.R;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +18,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Set up the ActionBar with a Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // menu should be considered as top-level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_maps, R.id.navigation_settings, R.id.navigation_alerts)
                 .build();
@@ -34,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     // If you want to support the Up button in the ActionBar
 
     private AppBarConfiguration appBarConfiguration;
+
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
