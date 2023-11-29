617b1acbf9b8e348ecc3929f40285ce5f8a8c22d
Parisa Rabbany <67644299+LadyPary@users.noreply.github.com>
2023-11-29 13:47:18 -0600

    work on the map

--- app/src/main/java/com/example/illinoiswatch/SettingsFragment.java
+++ app/src/main/java/com/example/illinoiswatch/SettingsFragment.java
@@ -8,33 +8,42 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
-
 import androidx.fragment.app.Fragment;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.Context;
 
 public class SettingsFragment extends Fragment {
 
     private ToggleButton toggleButton;
     private EditText editTextNumber;
     private EditText editTextAddress;
+    private TextView textViewSafetyRadius; // Make sure this is declared inside the class
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
-        // Initialize views
         toggleButton = view.findViewById(R.id.toggleButton);
         editTextNumber = view.findViewById(R.id.editTextNumber);
         editTextAddress = view.findViewById(R.id.editTextText);
+        textViewSafetyRadius = view.findViewById(R.id.textViewSafetyRadius); // Correctly placed
 
-        // Example of handling button click
+        // Handling button clicks
         Button buttonSettings1 = view.findViewById(R.id.btnSettings1);
-        buttonSettings1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle the button click for "Set Safety Radius"
-                // Add your logic here
-            }
+        buttonSettings1.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), MapsActivity.class);
+            startActivity(intent);
         });
+
+
+        if (getActivity() != null) {
+            SharedPreferences sharedPref = getActivity().getSharedPreferences("AppSettings", Context.MODE_PRIVATE);
+            int radiusInMeters = sharedPref.getInt("radius", 100); // Default value
+            double radiusInMiles = radiusInMeters * 0.000621371; // Convert meters to miles
+            // Use radiusInMiles as needed
+        }
 
         // Example of handling toggle button state change
         toggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
@@ -43,16 +52,29 @@ public class SettingsFragment extends Fragment {
             // Add your logic here
         });
 
-        // Example of handling save button click
         Button buttonSave = view.findViewById(R.id.btnSave);
-        buttonSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle the button click for "Save"
-                // Add your logic here
-            }
+        buttonSave.setOnClickListener(v -> {
+            // Handle save action
         });
+
+        updateRadiusDisplay();
 
         return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateRadiusDisplay();
+    }
+
+    private void updateRadiusDisplay() {
+        if (getActivity() != null) {
+            SharedPreferences sharedPref = getActivity().getSharedPreferences("AppSettings", Context.MODE_PRIVATE);
+            int radiusInMeters = sharedPref.getInt("radius", 100); // Default value in meters
+            double radiusInMiles = radiusInMeters * 0.000621371; // Convert meters to miles for display
+            textViewSafetyRadius.setText(String.format("Safety Radius: %.2f miles", radiusInMiles));
+        }
+    }
+
 }
