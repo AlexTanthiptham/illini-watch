69efa8b012081b8592a2dcfc2852b57bcf6198db
Alex Tanthiptham <jirawatcharat@gmail.com>
2023-11-29 14:26:31 -0600

    Dynamic population test

--- app/src/main/java/com/example/illinoiswatch/Database.java
+++ app/src/main/java/com/example/illinoiswatch/Database.java
@@ -10,8 +10,10 @@ public class Database {
 
     static {
         dataAlerts = new ArrayList<>();
-        dataAlerts.add(new DataAlert("Active", "10 km", "Alert 1", "Description 1", "10:00 AM", "Location 1"));
-        dataAlerts.add(new DataAlert("Inactive", "5 km", "Alert 2", "Description 2", "12:30 PM", "Location 2"));
+        dataAlerts.add(new DataAlert("Active", "10 mi", "Alert 1", "Description 1", "10:00 AM", "Location 1"));
+        dataAlerts.add(new DataAlert("Inactive", "5 mi", "Alert 2", "Description 2", "12:30 PM", "Location 2"));
+        dataAlerts.add(new DataAlert("Inactive", "3 mi", "Alert 3", "Description 4", "1:30 PM", "Location 3"));
+        dataAlerts.add(new DataAlert("Inactive", "4 mi", "Alert 4", "Description 5", "4:50 PM", "Location 4"));
         // Add more dataAlerts as needed
     }
 
